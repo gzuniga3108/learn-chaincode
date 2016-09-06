0508c904173e9dcdc7d0fb088789b88cd1cf5522
gzuniga3108 <genaro.zuniga@emergys.com>
2016-09-06 09:45:56 -0500

    Add files via upload

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,30 +1,28 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"strings"
 )
+
+var accountPrefix = "acct:"
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
+
+type Account struct {
+	ID          string  `json:"id"`
+	Prefix      string  `json:"prefix"`
+	CashBalance float64 `json:"cashBalance"`
+	AssetsIds   []string `json:"assetIds"`
+}
+
+
 
 // ============================================================================================================================
 // Main
@@ -55,8 +53,10 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
-	} else if function == "write" {
+	}else if function == "write" {
 		return t.write(stub,args)
+	}else if function == "createAccount" {
+		return t.createAccount(stub,args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 	return nil, errors.New("Received unknown function invocation")
@@ -79,7 +79,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 func (t *SimpleChaincode) write(stub *shim.ChaincodeStub,args []string)([]byte, error){
 	var key,value string
 	var err error
-	fmt.Println("reunning write()")
+	fmt.Println("running write()")
 	if len(args) != 2 {
 		return nil,errors.New("Incorrect number of arguments. Expecting 2")
 	}
@@ -108,4 +108,65 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub,args []string)([]byte,er
 	}
 
 	return valAsbytes,nil
+}
+
+func (t *SimpleChaincode) createAccount(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+    // Obtain the username to associate with the account
+    if len(args) != 1 {
+        fmt.Println("Error obtaining username")
+        return nil, errors.New("createAccount accepts a single username argument")
+    }
+    username := args[0]
+    
+    // Build an account object for the user
+    var assetIds []string
+    suffix := "000A"
+    prefix := username + suffix
+    var account = Account{ID: username, Prefix: prefix, CashBalance: 10000000.0, AssetsIds: assetIds}
+    accountBytes, err := json.Marshal(&account)
+    if err != nil {
+        fmt.Println("error creating account" + account.ID)
+        return nil, errors.New("Error creating account " + account.ID)
+    }
+    
+    fmt.Println("Attempting to get state of any existing account for " + account.ID)
+    existingBytes, err := stub.GetState(accountPrefix + account.ID)
+	if err == nil {        
+        var company Account
+        err = json.Unmarshal(existingBytes, &company)
+        if err != nil {
+            fmt.Println("Error unmarshalling account " + account.ID + "\n--->: " + err.Error())
+            
+            if strings.Contains(err.Error(), "unexpected end") {
+                fmt.Println("No data means existing account found for " + account.ID + ", initializing account.")
+                err = stub.PutState(accountPrefix+account.ID, accountBytes)
+                
+                if err == nil {
+                    fmt.Println("created account" + accountPrefix + account.ID)
+                    return nil, nil
+                } else {
+                    fmt.Println("failed to create initialize account for " + account.ID)
+                    return nil, errors.New("failed to initialize an account for " + account.ID + " => " + err.Error())
+                }
+            } else {
+                return nil, errors.New("Error unmarshalling existing account " + account.ID)
+            }
+        } else {
+            fmt.Println("Account already exists for " + account.ID + " " + company.ID)
+		    return nil, errors.New("Can't reinitialize existing user " + account.ID)
+        }
+    } else {
+        
+        fmt.Println("No existing account found for " + account.ID + ", initializing account.")
+        err = stub.PutState(accountPrefix+account.ID, accountBytes)
+        
+        if err == nil {
+            fmt.Println("created account" + accountPrefix + account.ID)
+            return nil, nil
+        } else {
+            fmt.Println("failed to create initialize account for " + account.ID)
+            return nil, errors.New("failed to initialize an account for " + account.ID + " => " + err.Error())
+        }
+        
+    }      
 }
